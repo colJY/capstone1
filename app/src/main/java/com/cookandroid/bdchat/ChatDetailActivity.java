d35bfe78899a80a4382f437c7511a2b76ea2d9d5
이종윤 <ljy162518@gmail.com>
2022-03-22 18:16:29 +0900

    update comments

---
+++ app/src/main/java/com/cookandroid/bdchat/ChatDetailActivity.java
@@ -0,0 +1,125 @@
+package com.cookandroid.bdchat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.cookandroid.bdchat.Adapter.ChatAdapter;
+import com.cookandroid.bdchat.Models.MessageModel;
+import com.cookandroid.bdchat.databinding.ActivityChatDetailBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class ChatDetailActivity extends AppCompatActivity {
+
+    ActivityChatDetailBinding binding;
+    FirebaseDatabase database;
+    FirebaseAuth auth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_detail);
+        binding = ActivityChatDetailBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        getSupportActionBar().hide();
+        database = FirebaseDatabase.getInstance();
+        auth = FirebaseAuth.getInstance();
+
+        final String senderId= auth.getUid();
+        String recieveId = getIntent().getStringExtra("userId"); // userId, userName 받아오기
+        String userName = getIntent().getStringExtra("userName");
+        String profilePic = getIntent().getStringExtra("profilePic"); // 추후 프로필 사진 사용할때 사용할 예정
+
+        binding.userName.setText(userName);
+        // 보내는 버튼
+        binding.backArrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ChatDetailActivity.this,MainActivity.class); // 뒤로가기 버튼 눌렀을 때 화면 전환
+                startActivity(intent);
+
+            }
+
+
+        });
+
+        // 메시지와 메시지 ui연결
+        final ArrayList<MessageModel> messageModels = new ArrayList<>();
+        final ChatAdapter chatAdapter = new ChatAdapter(messageModels,this,recieveId);
+
+        binding.chatRecyclerView.setAdapter(chatAdapter);
+
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this); // 수평,수직으로 배치시켜주는 레이아웃 매니저
+        binding.chatRecyclerView.setLayoutManager(layoutManager);
+
+        final String senderRoom = senderId + recieveId; // senderId와 recieveId 순서로 합친 String으로 Firebase database의 senderRoom과 receiverRoom을 나눈다.
+        final String receiverRoom = recieveId + senderId;
+
+        database.getReference().child("chats").child(senderRoom) // 앞서 users처럼 "chat"에 senderRoom에 저장
+                .addValueEventListener(new ValueEventListener() { // 경로의 전체 내용에 대한 변경 사항을 읽고 수신 대기합니다.
+
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        messageModels.clear(); // 배열을 리셋시킨다.
+                        for(DataSnapshot snapshot1 : snapshot.getChildren()){ // for문을 이용해 dataSnapshot 안의 내용을 처음부터 끝까지 꺼낼 수 있는 문장을 만들고, 그것은 snapshot이라는 변수에 들어간다
+                            MessageModel model = snapshot1.getValue(MessageModel.class);
+                            model.setMessageId(snapshot1.getKey());
+                            messageModels.add(model);
+                        }
+                        chatAdapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+
+        // 메시지 보내기 클릭 버튼
+        binding.send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String message = binding.enterMessage.getText().toString();
+                final MessageModel model = new MessageModel(senderId,message);
+                model.setTimestamp(new Date().getTime());
+                binding.enterMessage.setText("");
+
+                // real time DB에 User 밑에 chat에 생성 및 받는이 보낸이 채팅 연동
+                database.getReference().child("chats")
+                        .child(senderRoom).push()
+                        .setValue(model)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        database.getReference().child("chats").child(receiverRoom).push()
+                                .setValue(model).addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void unused) {
+
+                            }
+                        });
+                    }
+                });
+
+            }
+        });{
+
+        }
+
+    }
+}
---
+++ app/src/main/java/com/cookandroid/bdchat/SignInActivity.java
@@ -0,0 +1,105 @@
+package com.cookandroid.bdchat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.cookandroid.bdchat.Models.Users;
+import com.cookandroid.bdchat.databinding.ActivitySignInBinding;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class SignInActivity extends AppCompatActivity {
+
+    ActivitySignInBinding binding;
+    ProgressDialog progressDialog;
+    FirebaseAuth mAuth;
+    FirebaseDatabase firebaseDatabase;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+
+        getSupportActionBar().hide();
+
+        mAuth = FirebaseAuth.getInstance(); // Firebase의 인증 Instance를 받아온다.
+        firebaseDatabase = FirebaseDatabase.getInstance(); // firebse의 Database Instance를 가져온다
+
+        progressDialog = new ProgressDialog(SignInActivity.this);
+        progressDialog.setTitle("로그인 중");
+        progressDialog.setMessage("잠시만 기다려주세요\n 실행중입니다."); // 로그인 프로그레스 메시지 설정
+
+
+        binding.btnSignIn.setOnClickListener(new View.OnClickListener() {  //btnSignIn 버튼을 클릭했을 때
+            @Override
+            public void onClick(View view) {
+                if(!binding.txtEmail.getText().toString().isEmpty() && !binding.txtPassword.getText().toString().isEmpty()) // edit text 부분이 비어있지 않다면 아래를 실행
+                {
+                    progressDialog.show(); // 위에서 설정한 progressDialog를 보여주고,
+                    mAuth.signInWithEmailAndPassword(binding.txtEmail.getText().toString(),binding.txtPassword.getText().toString()) //비밀번호 기반의 계정을 로그인 하기 위해 signInWithEmailAndPassword를 사용하고,
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() { // 성공 유무를 확인한 후
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) { // progressDialog를 닫고, task가 성공했다면, if 아래를 실행시킨다.
+                                    progressDialog.dismiss();
+                                    if(task.isSuccessful()){
+                                        Intent intent = new Intent(SignInActivity.this,MainActivity.class); // SignInActivity에서 MainActivity로 화면을 전환한다,
+                                        startActivity(intent);
+                                    }
+                                    else
+                                    {
+                                        Toast.makeText(SignInActivity.this, task.getException().getMessage(), Toast.LENGTH_SHORT).show(); // task 실패하면 Toast메시지를 출력한다.
+                                    }
+                                }
+                            });
+                }
+                else
+                {
+                    Toast.makeText(SignInActivity.this, "이메일 / 패스워드를 입력하세요", Toast.LENGTH_SHORT).show();// edit text 부분이 비어있다면 Toast 메시지 출력
+                }
+            }
+        });
+
+        // 로그인 이후 계속 로그인 유지
+        if(mAuth.getCurrentUser()!=null)
+        {
+            Intent intent = new Intent(SignInActivity.this,MainActivity.class);
+            startActivity(intent);
+
+        }
+        // 회원가입 화면으로 이동
+        binding.txtClickSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignInActivity.this,SingUpActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+
+
+}
